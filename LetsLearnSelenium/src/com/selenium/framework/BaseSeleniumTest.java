5485cf24fe506621885f7f2a1e35bbe01ea62e88
ChandanGupta <ChandanGupta@INDP277LT.mshome.net>
2021-09-25 11:05:59 +0530

    Added Extent report and context menu

--- LetsLearnSelenium/src/com/selenium/framework/BaseSeleniumTest.java
+++ LetsLearnSelenium/src/com/selenium/framework/BaseSeleniumTest.java
@@ -3,12 +3,25 @@ package com.selenium.framework;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class BaseSeleniumTest {
 
 	WebDriver driver;
+	ExtentReports report;
+	ExtentTest test;
+
+	@BeforeClass
+	public void oneTimeSetUp() {
+		report = new ExtentReports("C:\\Test\\ExtentReport" + System.currentTimeMillis() + ".html", true);
+		test = report.startTest("ExtentDemo");
+	}
 
 	@BeforeMethod
 	public void init() {
@@ -17,12 +30,18 @@ public class BaseSeleniumTest {
 		driver = new ChromeDriver();
 		driver.manage().window().setPosition(new Point(2000, 100));
 		driver.manage().window().maximize();
-		driver.get("https://the-internet.herokuapp.com/login");
+		// test.log(LogStatus.INFO, "Navigated to the URL");
 	}
 
 	@AfterMethod
 	public void tearDown() {
-		// driver.quit();
+		driver.quit();
+	}
+
+	@AfterClass
+	public void endTest() {
+		report.endTest(test);
+		report.flush();
 	}
 
 }
--- LetsLearnSelenium/src/com/selenium/framework/MouseKeyBoardOps.java
+++ LetsLearnSelenium/src/com/selenium/framework/MouseKeyBoardOps.java
@@ -1,35 +1,56 @@
 package com.selenium.framework;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.LogStatus;
+
 public class MouseKeyBoardOps extends BaseSeleniumTest {
+
+	@Test
+	public void contextMenu() {
+		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
+		test.log(LogStatus.INFO, "Navigated to the URL was successful");
+		Actions actions = new Actions(driver);
+		WebElement rightClickBtn = driver.findElement(By.xpath("//span[text()='right click me']"));
+		actions.contextClick(rightClickBtn).perform();
+		test.log(LogStatus.INFO, "Right click button was right clicked using selenium context menu");
+		driver.findElement(By.xpath("//li/span[text()='Edit']")).click();
+		test.log(LogStatus.INFO, "Click on Edit");
+		Alert alert = driver.switchTo().alert();
+		test.log(LogStatus.INFO, "Switching to an Alert");
+		alert.accept();
+		test.log(LogStatus.INFO, "Closing the Alert");
+	}
 
 	// @Test
 	public void MouseKeyActions() {
 
+		driver.get("https://the-internet.herokuapp.com/login");
 		Actions actions = new Actions(driver);
 		WebElement link = driver.findElement(By.xpath("//a[text()='Elemental Selenium']"));
 
 		actions.moveToElement(link).click(link).perform();
-		// actions.click(link);
-		// actions.perform();
+		test.log(LogStatus.INFO, "Clicked on Elemental Selenium");
 	}
 
-	@Test
+	// @Test
 	public void keyboardAction() {
+		driver.get("https://the-internet.herokuapp.com/login");
 
 		Actions actions = new Actions(driver);
 		WebElement username = driver.findElement(By.id("username"));
 
 		actions.keyDown(username, Keys.SHIFT);
 		actions.sendKeys("tomsmith");
-		// actions.keyUp(Keys.UP);
 		actions.keyUp(Keys.SHIFT);
+		actions.doubleClick();
 		actions.perform();
+		test.log(LogStatus.INFO, "DoubleClick was successful");
 
 	}
 }
